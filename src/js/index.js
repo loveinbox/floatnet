edfde82a6e9945bee6ae966b463a9fc511971584
hank <dhuhank@foxmail.com>
2017-07-01 07:58:49 +0800

    add basic render

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,21 @@
+const data = {
+  index: [
+    [1, 2, 3],
+    [4, 5, 6]
+  ]
+}
+
 class PageRender {
   constructor(width) {
     console.log(123)
   }
+
+  render(data, containerSelector) {
+    const container = document.querySelector(containerSelector)
+    data.forEach(item => {
+      container.innerText += item
+    })
+  }
 }
 
-
-let pr = new PageRender(450)
+new PageRender(450).render(data.index, '.pics')
