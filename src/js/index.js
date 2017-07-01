4fccb1ac219edddceb1940513687ebf55d51c174
hank <dhuhank@foxmail.com>
2017-07-01 08:32:14 +0800

    update basic render

--- src/js/index.js
+++ src/js/index.js
@@ -7,15 +7,49 @@ const data = {
 
 class PageRender {
   constructor(width) {
-    console.log(123)
+    if (width <= 450) {
+      this.listContainer = document.createElement('ul')
+      this.listContainer.className = 'matrix-container'
+
+      this.createListGroup = () => {
+        return document.createElement('ul')
+      }
+      this.createListItem = data => {
+        let item = document.createElement('li')
+        item.innerText = data
+        return item
+      }
+    } else {
+      this.listContainer = document.createElement('table')
+      this.listContainer.className = 'matrix-container'
+
+      this.createListGroup = () => {
+        return document.createElement('tr')
+      }
+      this.createListItem = data => {
+        let item = document.createElement('td')
+        item.innerText = data
+        item.setAttribute('data-123', data)
+        return item
+      }
+    }
   }
 
   render(data, containerSelector) {
     const container = document.querySelector(containerSelector)
-    data.forEach(item => {
-      container.innerText += item
+
+    data.forEach(list => {
+      let listGroup = this.createListGroup(list)
+      let listItem
+      list.forEach(listItem => {
+        listItem = this.createListItem(listItem)
+        listGroup.appendChild(listItem)
+      })
+      this.listContainer.appendChild(listGroup)
     })
+
+    container.appendChild(this.listContainer)
   }
 }
 
-new PageRender(450).render(data.index, '.pics')
+new PageRender(window.innerWidth).render(data.index, '.pics')
